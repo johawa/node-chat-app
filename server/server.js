5414d64359238adaa51995a1bb50e6bb20185616
timebasher <johannes.walenta@googlemail.com>
2018-04-25 11:27:29 +0200

    add basic socket.io eventlisteners

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
-const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http'); //build in Node
+const path = require('path'); //build in Node
 const express = require('express');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-const app = express();
+var app = express();
+var server = http.createServer(app); // made up to use socket.io
+var io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
 
-app.listen(port, () => {
+//Set up Eventlistener for Socket.io for Serverside
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    //Set up disconnect Eventlistener for Serverside
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Server is listening on Port ${port}`);
 });
