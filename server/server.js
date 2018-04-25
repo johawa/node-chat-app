f70cad3bedb3b39c6e60d212f1d9ac7c876c7941
timebasher <johannes.walenta@googlemail.com>
2018-04-25 14:35:36 +0200

    create msg events

--- server/server.js
+++ server/server.js
@@ -1,30 +1,34 @@
-const socketIO = require('socket.io');
-const http = require('http'); //build in Node
-const path = require('path'); //build in Node
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-
 var app = express();
-var server = http.createServer(app); // made up to use socket.io
+var server = http.createServer(app);
 var io = socketIO(server);
-
 
 app.use(express.static(publicPath));
 
-
-//Set up Eventlistener for Socket.io for Serverside
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log('New user connected');
+  //emit to clien
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+  //reveive from client
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
 
-    //Set up disconnect Eventlistener for Serverside
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-
 server.listen(port, () => {
-    console.log(`Server is listening on Port ${port}`);
+  console.log(`Server is up on ${port}`);
 });
