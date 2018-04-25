8d6c4766fe73f01a74cc5f93c00c61a4f58563f8
timebasher <johannes.walenta@googlemail.com>
2018-04-25 17:10:22 +0200

    add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,29 @@
 var socket = io();
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+    console.log('Connected to server');
 });
 
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+    console.log('Disconnected from server');
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+    console.log('newMessage', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+
+jQuery('#message-form').on('submit', function (event) {
+    event.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+       
+    });
 });
--- server/server.js
+++ server/server.js
@@ -19,9 +19,11 @@ io.on('connection', (socket) => {
   
   //emiting to everybody expect one user
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
-  socket.on('createMessage', (message) => {
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback(message);
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
